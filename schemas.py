763fe53dfaa718f827e2223c29550c2df0fb080b
Tadeo Bennett <tadeos.bennett@gmail.com>
2024-09-05 15:44:04 -0600

    Update models with sqlAlchemy

--- schemas.py
+++ schemas.py
@@ -19,6 +19,7 @@ class ItemSchema(PlainItemSchema):    # extends the PlainItemSchema
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
+    store_id = fields.Int()
 
 
 class StoreSchema(PlainStoreSchema):    # extends the PlainItemSchema
