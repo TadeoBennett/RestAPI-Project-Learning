2be4b5dae08942f1dec03be6b8dd8de1a32d7f6c
Tadeo Bennett <tadeos.bennett@gmail.com>
2024-09-13 13:56:48 -0600

    Add settings file for rq worker

--- tasks.py
+++ tasks.py
@@ -1,19 +1,27 @@
 import os
 import requests
+import jinja2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
+template_loader=jinja2.FileSystemLoader('templates')
+template_env = jinja2.Environment(loader=template_loader)
 
-def send_simple_message(to, subject, body):
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+
+def send_simple_message(to, subject, body, html):
     return requests.post(
         f"https://api.mailgun.net/v3/{DOMAIN}/messages",
         auth=("api", os.getenv("MAILGUN_API_KEY")),
         data={"from": f"Your Name <mailgun@{DOMAIN}>",
             "to": [to],
             "subject": subject,
-            "text": body}
+            "text": body,
+            "html": html}
     )
     
 def send_user_registration_email(email, username):
@@ -22,5 +30,6 @@ def send_user_registration_email(email, username):
         "Successfully signed up",
         f"Hi {username}! You have successfully registered to the Stores REST API. Thank you for trying it out! "
         f"Use the log in/authenticate endpoint to get an access token for using the other endpoints. "
-        f"Use the access token in the Authorization header of your requests by clicking the 'Authorize' button on the top of the swagger-ui docs page and pasting the token into the input on the displayed modal."
+        f"Use the access token in the Authorization header of your requests by clicking the 'Authorize' button on the top of the swagger-ui docs page and pasting the token into the input on the displayed modal.",
+        render_template("email/action.html", username=username)
     )
